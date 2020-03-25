efa090483fcfdb3d78e4e3e43183b0695d6152b6
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-03-24 22:39:48 -0600

    Docs for tasks

--- api_covid19/tasks.py
+++ api_covid19/tasks.py
@@ -3,6 +3,8 @@
 ========================
 Celery Tasks
 ========================
+For more celery crontab info visit: 
+    https://docs.celeryproject.org/en/latest/userguide/periodic-tasks.html#crontab-schedules
 """
 
 # celery imports
@@ -15,13 +17,19 @@ from .scripts.fetch_data import run
 
 @periodic_task(run_every=(crontab(hour=2)), name="update_info: Scrap https://www.gob.mx/ website every 2 hrs")
 def update_info():
+    """
+    This is a real world task.
+    """
     print("Updating data...")
     run()
 
 @periodic_task(run_every=(crontab()), name="example_task: This is example task running every minute")
 def example_task():
+    """
+    This is a example task
+    """
     # do some stuff
     # use models
     # make requests
     print("Running every minute...")
-    run()
+    run() # just for test. May be deleted later
