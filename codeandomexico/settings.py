f551fec0ead6269995f075afeabc3bd1f7a1ac8a
Cesar Soriano <cesar.f.soriano@gmail.com>
2020-04-08 17:07:34 -0500

    static root add apicovid_19

--- codeandomexico/settings.py
+++ codeandomexico/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'api_covid19'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( 'static', )
 
